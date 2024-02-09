f8f5f7659d6f46bb2f81da8e701a029349954bf3
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2024-02-09 03:41:38 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Person from './components/Person'
+import Index from "./components/Index";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  )
+}
+
+export default App
---
+++ src/test/Index.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import '@testing-library/jest-dom'
+import Index   from "../components/Index";
+
+// render Index component without crashing
+test("Renders Index component correctly", async () => {
+    const { getByText } = render(<Index/>);
+
+    const htmlElement = getByText('cinema app')
+
+    expect(htmlElement).not.toBeFalsy()
+});
